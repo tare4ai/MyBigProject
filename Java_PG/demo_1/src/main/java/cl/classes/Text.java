bf82a8749b32ce91792dd74c981de8a84ac07b33
tare4ai <tarasik.sumy@gmail.com>
2025-01-09 14:17:23 +0200

    обновил таск

--- Java_PG/demo_1/src/main/java/cl/classes/Text.java
+++
@@ -1,5 +0,0 @@
-package cl.classes;
-
-public class Text {
-
-}
--- Java_PG/demo_1/src/main/java/my/program/App.java
+++ Java_PG/demo_1/src/main/java/my/program/App.java
@@ -1,13 +1,18 @@
+import java.util.Scanner
 package my.program;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner in =  new Scanner(System.in);
+        double x1, x2, y1, y2, r1, r2;
+        x1 = in.nextDouble();
+        y1 = in.nextDouble();
+        r1 = in.nextDouble();
+        x2 = in.nextDouble();
+        y2 = in.nextDouble();
+        r2 = in.nextDouble();
+        System.out.println( "%d", r2 );
     }
 }
